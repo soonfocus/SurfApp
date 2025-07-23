72af9480868ca06f7a93188ceb5fab1ce98269d1
PiotrGrzechnik <ppgrzechnik@gmail.com>
2025-07-23 16:43:11 +0200

    Add prep screen, countdown, etc

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,56 @@
 /* Menu */
 const menu = document.getElementById('menu')
 const rideContainer = document.getElementById('ride-container')
-const backButton = document.getElementById('btn-back')
+const prepScreen = document.getElementById('prep-screen')
 
+/* Back button */
+const backButton = document.getElementById('btn-back')
 backButton.addEventListener('click', () => {
   location.reload();
 })
 
-
 SurferMng = new SurferManager();
 
 
-
-/* Start ride */
-const startCommonActions = () => {
+/* Prep screen */
+const startCommonActions = (TrackMng) => {
   menu.style.display = 'none';
-  rideContainer.style.display = 'block';
+  prepScreen.style.display = 'flex';
+  const prepScreenCounter = document.getElementById('counter')
+
+  countDown(5, prepScreenCounter).then((result) => {
+    /* Start ride */
+    if (result) {
+      prepScreen.style.display = 'none';
+      rideContainer.style.display = "block";
+
+      SurferMng.startDetectingPosition()
+      const surferElement = SurferMng.getSurferElement();
+      TrackMng.setSurferElement(surferElement);
+
+      TrackMng.start();
+    }
+  });
 }
 
+
+
+/* Random Track */
 const btnStartRandom = document.getElementById('btn-start-random')
 btnStartRandom.addEventListener('click', () => {
-  startCommonActions()
-
+  
   const {elements, elementsReps, speed, delayAll} = randomOneTrack
   const TrackMng = new TrackManager('#track', elements, elementsReps, speed, delayAll);
-
-  SurferMng.startDetectingPosition()
-  const surferElement = SurferMng.getSurferElement();
-  TrackMng.setSurferElement(surferElement);
-  TrackMng.start();
+  
+  startCommonActions(TrackMng)
 })
 
+/* Pumping Track */
 const btnStartPumping = document.getElementById('btn-start-pumping')
 btnStartPumping.addEventListener('click', () => {
-  startCommonActions()
-
+  
   const {elements, elementsReps, speed, delayAll} = pumpingOneTrack
   const TrackMng = new TrackManager('#track', elements, elementsReps, speed, delayAll);
-
-  SurferMng.startDetectingPosition()
-  const surferElement = SurferMng.getSurferElement();
-  TrackMng.setSurferElement(surferElement);
-  TrackMng.start();
+  
+  startCommonActions(TrackMng)
 })
